c114faf030369b39ea9e7db03eba6d4672823214
Sam Hunt <huntsamuel2002@gmail.com>
2023-01-04 14:09:16 +0000

    css stuff coming

--- Acebook/Controllers/PostsController.cs
+++ Acebook/Controllers/PostsController.cs
@@ -2,6 +2,8 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using acebook.Models;
 using acebook.ActionFilters;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace acebook.Controllers;
 
@@ -17,24 +19,35 @@ public class PostsController : Controller
 
     [Route("/posts")]
     [HttpGet]
-    public IActionResult Index() {
-      AcebookDbContext dbContext = new AcebookDbContext();
-      List<Post> posts = dbContext.Posts.ToList();
-      posts.Reverse();
-      ViewBag.Posts = posts;
-      return View();
+    public IActionResult Index()
+    {
+        AcebookDbContext dbContext = new AcebookDbContext();
+        var posts = dbContext.Posts
+          .Include(p => p.User)  // Include user data for each post
+          .ToList();
+        posts.Reverse();
+        ViewBag.Posts = posts;
+        return View();
     }
 
     [Route("/posts")]
-    [HttpPost]
-    public RedirectResult Create(Post post) {
+[HttpPost]
+public IActionResult Create(Post post) {
+  if (string.IsNullOrWhiteSpace(post.Content))
+  {
+      return RedirectToAction("Index");
+  }
+  else
+  {
       AcebookDbContext dbContext = new AcebookDbContext();
       int currentUserId = HttpContext.Session.GetInt32("user_id").Value;
       post.UserId = currentUserId;
       dbContext.Posts.Add(post);
       dbContext.SaveChanges();
-      return new RedirectResult("/posts");
-    }
+      return RedirectToAction("Index");
+  }
+}
+
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
